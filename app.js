bae3c8b1079d571bede12c545d145c1edc33f9d3
triana <trianaheinz@gmail.com>
2021-04-13 17:06:25 +0200

    until iteration 4.2 done

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname + '/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,6 +20,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then(beersFromApi =>{
+      res.render('beers',{ beersFromApi })
+
+  })
+    .catch(error => console.log(error));
+
+  //res.render('beers');
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', {responseFromAPI})
+      console.log('Response from API: ', responseFromAPI);
+    })
+    .catch(error => console.log(error));
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
